3cb8b4a3c43ba575ad9a62648bdbad7ff5ffd5e6
John Bender <john.m.bender@gmail.com>
2010-03-20 17:56:38 -0700

    a bit more documentation

--- lib/mario/hats/nix.rb
+++ lib/mario/hats/nix.rb
@@ -2,6 +2,11 @@
 module Mario
   module Hats
     module Nix
+      
+      # Escapes paths for use in execute statements
+      # 
+      # @param [String]
+      # @return [String]
       def shell_escape_path(str)
         # Taken from ruby mailing list, seems to work, probably needs lots of testing :(
         str.to_s.gsub(/(?=[^a-zA-Z0-9_.\/\-\x7F-\xFF\n])/n, '\\').
--- lib/mario/hats/windows.rb
+++ lib/mario/hats/windows.rb
@@ -1,6 +1,9 @@
 module Mario
   module Hats
     module Windows
+      # Escapes a file paths for use in system calls and execute statements
+      # @param [String]
+      # @return [String]
       def shell_escape_path(str)
         #Wrap windows paths with white space in quotes
         str =~ /\s/ ? "\"#{str}\"" : str
