caf8110ba43e3b59a7b9cae9c9077388660c2111
John Bender <john.m.bender@gmail.com>
2010-03-20 17:47:11 -0700

    docs update on platform

--- lib/mario/platform.rb
+++ lib/mario/platform.rb
@@ -5,20 +5,26 @@ module Mario
     @@current = nil
 
     class << self
-      # TODO debatable whether cygwin should be here, initial thoughts is that it should be
+      
+      # A list of unix like operating system classes
+      #
+      # @return [Array[Class]]
       def nix_group
         [Cygwin, Linux, BSD, Solaris, Darwin]
       end
       
+      # A list of windows operating system classes
+      #
+      # @return [Array[Class]]
       def windows_group
         [Windows7, WindowsNT]
       end
 
-      # A hash of os target internel names and partial strings that will be checked against Config::CONFIG['target_os']
+      # The union of the {nix_group} and {windows_group} operating system class sets
       # 
-      # @return [Hash]
+      # @return [Array[Class]]
       def targets
-        nix_group + windows_group
+        nix_group | windows_group
       end
 
       # Checks if the current platform is linux
@@ -42,30 +48,51 @@ module Mario
         check Solaris
       end
 
+      # Checks if the current platform is bsd
+      #
+      # @return [true, false]
       def bsd?
         check BSD
       end
       
+      # Checks if the current platform is cygwin
+      #
+      # @return [true, false]
       def cygwin?
         check Cygwin
       end
 
+      # Checks if the current platform is windows 7
+      #
+      # @return [true, false]
       def windows7?
         check Windows7
       end
       
+      # Checks if the current platform is windows nt
+      #
+      # @return [true, false]
       def windowsnt?
         check WindowsNT
       end
 
+      # Checks if the current platform is part of the {nix_group} and returns that class
+      #
+      # @return [Class]
       def nix?
         check_group nix_group
       end
 
+      # Checks if the current platform is part of the {windows_group} and returns that class
+      #
+      # @return [Class]
       def windows?
         check_group windows_group
       end
 
+      # Checks a list of possible operating system classes to see if their target os strings match the {target_os} value
+      #
+      # @return [Class]
       def check_group(group)
         group.each do |klass|
           return klass if check klass
@@ -75,35 +102,49 @@ module Mario
 
       alias_method :osx?, :darwin?
 
-      # Uses the forced os class if provided otherwise uses the target_os rbconfig hash element
+      # Uses the forced class target os string if provided otherwise uses the target_os rbconfig hash element
       #
       # @return [String]
       def target_os
         @@forced ? @@forced.target : Config::CONFIG['target_os']
       end
 
+      # Checks an os class against {target_os}
+      #
+      # @return [true, false]
       def check(klass)
         target_os.downcase.include?(klass.target)
       end
 
+      # Allows the user to force Mario to report the operating system as one of the provided operatin system classes
+      #
       def forced=(klass)
         @@current=nil
         @@forced=klass
         logger.warn(<<-msg)
 Mario::Platform.target_os will now report as '#{target_os}' and #{klass} will be used for all functionality including operating system checks and hat based functionality (See Mario::Hats for more information)
 msg
-
       end
       
+      # Returns the value of the currently forced operating system class if any
+      #
+      # @return [Class]
       def forced
         @@forced
       end
 
+      # Allows the setting of a logging mechanism, defaults to STDOUT
+      #
+      # @return [Logger]
       def logger(out=STDOUT)
         @@logger ||= Logger.new(out)
         @@logger
       end
 
+      # Returns an instance of the current operating system class as determined by {check_group} against all operating
+      # system classes provided by {targets}
+      #
+      # @return [OperatingSystem]
       def current
         # Search for the current target os
         current_target_klass = check_group(targets)
@@ -112,9 +153,7 @@ msg
       end
     end
 
-    
     # Any additional fucniontality and they should be moved to a lib/platforms/<OS>.rb
-
     class Cygwin
       include Hats::Nix
       
